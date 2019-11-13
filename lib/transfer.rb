ae9f2d23ae9ef795e77bcfd1b27a1053ef897ace
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-13 17:39:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
+  attr_accessor :status 
+  attr_reader :sender, :receiver
+  def initialize(sender, receiver)
+    @sender = sender 
+    @receiver = receiver 
+    @status = pending 
+  end
   # your code here
 end
